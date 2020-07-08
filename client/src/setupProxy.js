0db117e401b2990165f65f8104ec8fa7110d855b
xeron07 <ni.xeron07@gmail.com>
2020-07-08 11:53:45 +0600

    img link adding

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -3,7 +3,15 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
   app.use(
-    "/api",
+    "/api/*",
+    createProxyMiddleware({
+      target: "http://localhost:8000",
+      changeOrigin: true,
+    })
+  );
+
+  app.use(
+    "/img/*",
     createProxyMiddleware({
       target: "http://localhost:8000",
       changeOrigin: true,
